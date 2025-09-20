8d47fe8d1b006e9a97eb8771959d8f4469f52c33
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-20 22:36:01 +0530

    updated code

--- app/validate/page.tsx
+++ app/validate/page.tsx
@@ -1,14 +1,18 @@
-import { auth } from "@/auth"
+import { auth } from "@/auth";
+import ValidateBtn from "@/components/ValidateBtn";
+import { redirect } from "next/navigation";
 
-export default async function (){
-    const session = await auth()
-    if(!session?.user?.id){
-        return
-    }
-    return (
-        <div>
-            {session.user.name}
-        </div>
-    )
-
+export default async function () {
+  const session = await auth();
+  if (!session) {
+    redirect("/");
+  }
+  return (
+    <div className="w-full h-[92.5vh] text-center pt-40">
+      <div>
+        <h1 className="text-8xl font-bold ">I am a...</h1>
+        <ValidateBtn />
+      </div>
+    </div>
+  );
 }
