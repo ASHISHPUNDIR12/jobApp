391b9b841237afd90b832cac3fd1f90c71feaee9
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-20 22:35:32 +0530

    home page ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
-import { auth } from "@/auth";
-
+import MovingCompanies from "@/components/MovingCompanies";
+import Image from "next/image";
+import banner from "@/public/banner.jpeg";
+import Acordian from "@/components/Acordian";
+import Homebtn from "@/components/HomeBtn";
 export default async function Home() {
-  // const session = await auth();
-  // if (!session?.user?.id) {
-  //   return;
-  // }
   return (
-    <div>
-      this is home page
-      {/* <h1>{session.user.name}</h1> */}
+    <div className="">
+      <div className="text-center mt-30">
+        <p className="text-8xl font-bold">Find Your Dream Job</p>
+        <p className="text-8xl font-bold">and get Hired</p>
+        <p className="mt-10 font-semibold ">
+          Explore Thaosands of job listing or find the perfect candidate
+        </p>
+       <Homebtn/>
+        <div className="mt-15">
+          <MovingCompanies />
+        </div>
+        <Image className="mx-auto rounded " src={banner} alt="banner" />
+        <div className="flex justify-center gap-5 mt-10 ">
+          <div className="border border-black  py-5 px-28 ">
+            <p className="text-2xl font-semibold">For job Seeker</p>
+            <p>Search and apply for jobs, track applications, and more.</p>
+          </div>
+          <div className="border border-black  py-5 px-28">
+            <p className="text-2xl font-semibold">For Employers</p>
+            <p>Post jobs, manage applications, and find the best candidates.</p>
+          </div>
+        </div>
+        <div className=" max-w-[85%] mx-auto mt-10">
+          <Acordian/>
+        </div>
+      </div>
     </div>
   );
 }
