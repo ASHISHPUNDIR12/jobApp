05fa1c10e8494794e8f2c61e1f5e05daac6cf7f8
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-22 23:15:53 +0530

    changes in model

---
+++ prisma/migrations/20250921150506_add_job_image_with_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Job" ADD COLUMN     "image" TEXT NOT NULL DEFAULT '/companies/default.webp';
---
+++ prisma/migrations/20250921151727_edit/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Job" ALTER COLUMN "image" DROP NOT NULL;
