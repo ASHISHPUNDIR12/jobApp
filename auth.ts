a544eafcdf5cd7414906796a7d7e67af2aa004e3
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-25 10:08:10 +0530

    push

--- auth.ts
+++ auth.ts
@@ -6,5 +6,14 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 export const {handlers  , signIn , signOut , auth} = NextAuth({
     adapter : PrismaAdapter(prisma),
     providers : [Google],
-    debug:true
+    debug:true,
+    callbacks: {
+  session({ session, user }) {
+    if (session.user) {
+      session.user.id = user.id;
+      session.user.role = user.role; 
+    }
+    return session;
+  },
+},
 })
